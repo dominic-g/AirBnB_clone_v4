2f3251197a542f1203b3bda69be086f723afd64f
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-10-08 07:24:56 -0400

    API status Files

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  let amenities = {};
+
+  // Event listener for checkbox changes
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    let lst = Object.values(amenities);
+    if (lst.length > 0) {
+      $('div.amenities > h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+
+  // AJAX request to check API status
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+});
+
