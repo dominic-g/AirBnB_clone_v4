fb928ae358585e342a92fe2dee8feae843675fbd
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-10-08 16:45:21 -0400

    3 file(s)  created:  Pushed at 2023-10-08 16:45:21

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  let amenities = {};
+
+  // Event listener for checkbox changes
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    let lst = Object.values(amenities);
+    if (lst.length > 0) {
+      $('div.amenities > h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+
+  // AJAX request to check API status
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+
+  // AJAX request to fetch and display places data
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let pl = data[i];
+        $('.places ').append('<article><h2>' + pl.name + '</h2><div class="price_by_night"><p>$' + pl.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + pl.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + pl.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + pl.number_bathrooms + '</p></div></div><div class="description"><p>' + pl.description + '</p></div></article>');
+      }
+    }
+  });
+});
